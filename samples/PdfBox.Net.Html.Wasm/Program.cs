8c8218aff4ce8c76b19bb69ab8ba500e86bd095f
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2026-07-12 23:11:10 +0200

    Add browser-lite PDF-to-HTML WASM proof
    Closes #782

---
+++ samples/PdfBox.Net.Html.Wasm/Program.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PdfBox.Net.Html.Wasm;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
